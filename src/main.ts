064e6a4bb8dd5dac13afd270127c3a87d3229e71
Dmitrii Sorin <dsorin@atlassian.com>
2022-08-04 13:12:46 +1000

    Add run on every push to debug

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,13 @@ import { getNewVideos } from './youtube';
 import { Credentials } from 'google-auth-library';
 import { sendMesage } from './telegram';
 
-async function main() {}
+async function main() {
+  if (!process.env.GOOGLE_OAUTH_CREDENTIALS) {
+    throw new Error('GOOGLE_OAUTH_CREDENTIALS env is not set');
+  }
+
+  console.log('About to run...');
+}
 
 main().catch((err: Error) => {
   console.error(err.stack);
