574bd27a18e934397c064ab6963acfdad3a74e6f
Dmitrii Sorin <dsorin@atlassian.com>
2022-08-14 09:23:17 +1000

    Use artifacts

--- src/cli/youtube-fetch.ts
+++ src/cli/youtube-fetch.ts
@@ -6,36 +6,21 @@ async function main() {
   since.setHours(since.getHours() - 1);
   info(`Fetching videos since ${since}`);
 
-  // if (!process.env.GOOGLE_OAUTH_CLIENT_ID) {
-  //   throw new Error('GOOGLE_OAUTH_CLIENT_ID env is not set');
-  // }
-  //
-  // if (!process.env.GOOGLE_OAUTH_CLIENT_SECRET) {
-  //   throw new Error('GOOGLE_OAUTH_CLIENT_SECRET env is not set');
-  // }
-  //
-  // if (!process.env.GOOGLE_OAUTH_CLIENT_REDIRECT_URI) {
-  //   throw new Error('GOOGLE_OAUTH_CLIENT_REDIRECT_URI env is not set');
-  // }
-  //
-  // if (!process.env.GOOGLE_OAUTH_CREDENTIALS) {
-  //   throw new Error('GOOGLE_OAUTH_CREDENTIALS env is not set');
-  // }
+  if (!process.env.GOOGLE_OAUTH_CLIENT_ID) {
+    throw new Error('GOOGLE_OAUTH_CLIENT_ID env is not set');
+  }
 
-  process.env.GOOGLE_OAUTH_CLIENT_ID = '786228919943-fcln3p4nv863k65cdht7m2ltf8npke3e.apps.googleusercontent.com';
-  process.env.GOOGLE_OAUTH_CLIENT_SECRET = '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';
-  process.env.GOOGLE_OAUTH_CLIENT_REDIRECT_URI = 'http://localhost:3000/oauth2callback';
+  if (!process.env.GOOGLE_OAUTH_CLIENT_SECRET) {
+    throw new Error('GOOGLE_OAUTH_CLIENT_SECRET env is not set');
+  }
 
-  process.env.GOOGLE_OAUTH_CREDENTIALS = JSON.stringify({
-    access_token:
-      'ya29.A0AVA9y1tzF6ubid7BFAKlPeZiBMZJwFPyQnp_dde2KabzNn-3abDzwFLrvP7rKMRAzZ71o8kDi9YgyPyZ9wfmab2_AFB7vcMHAmx5mRt84qXTyGkO1tYxHTus-9RqcS9XHXTWSRt8cQyXHNUwTwPib6OGWEEgYUNnWUtBVEFTQVRBU0ZRRTY1ZHI4OUNzaURQN3B1T0hQbzhjVGlqS3NoQQ0163',
-    refresh_token:
-      '1//0gYSWBoY7u_dWCgYIARAAGBASNwF-L9IroyMDTHbf5JNvCU4qzMISRxqXFDX1TUTxQf_evnvdJ65J5vpyuuRZO8i6j15KpnziFGs',
-    scope:
-      'https://www.googleapis.com/auth/youtube.force-ssl https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/youtubepartner https://www.googleapis.com/auth/youtube.upload https://www.googleapis.com/auth/youtubepartner-channel-audit https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/youtube.channel-memberships.creator',
-    token_type: 'Bearer',
-    expiry_date: 1659447701173,
-  });
+  if (!process.env.GOOGLE_OAUTH_CLIENT_REDIRECT_URI) {
+    throw new Error('GOOGLE_OAUTH_CLIENT_REDIRECT_URI env is not set');
+  }
+
+  if (!process.env.GOOGLE_OAUTH_CREDENTIALS) {
+    throw new Error('GOOGLE_OAUTH_CREDENTIALS env is not set');
+  }
 
   const channelVideos = await getNewVideos(
     process.env.GOOGLE_OAUTH_CLIENT_ID,
@@ -48,8 +33,8 @@ async function main() {
   const output = new Array<string>();
   for (const { channel, videos } of channelVideos) {
     for (const video of videos) {
-      const message = `[${video.id}] ${channel.title} - ${video.title}`;
-      output.push(message);
+      const message = { channel, video };
+      output.push(JSON.stringify(message));
     }
   }
 
--- src/github.ts
+++ src/github.ts
@@ -1,13 +1,30 @@
+import { promises as fs } from 'fs';
+import { resolve } from 'path';
 import { Octokit } from '@octokit/core';
+import extractZip from 'extract-zip';
 
 // @see https://docs.github.com/en/rest/actions/artifacts#list-artifacts-for-a-repository
 type Artifact = {
   id: number;
+  name: string;
   archive_download_url: string;
   created_at: string;
 };
 
-export const getMostRecentArtifact = async (
+const readZipArchive = async (archive: ArrayBuffer, artifactName: string): Promise<string> => {
+  const { temporaryDirectory, temporaryFile } = await import('tempy');
+  const tmpArchiveFilePath = temporaryFile();
+  const tmpExtractDir = temporaryDirectory();
+  const view = new DataView(archive);
+
+  await fs.writeFile(tmpArchiveFilePath, view);
+  await extractZip(tmpArchiveFilePath, { dir: tmpExtractDir });
+
+  const extractedArtifactPath = resolve(tmpExtractDir, artifactName);
+  return (await fs.readFile(extractedArtifactPath, { encoding: 'utf-8' })).trim();
+};
+
+const getMostRecentArtifact = async (
   repoOwner: string,
   repoName: string,
   authToken: string,
@@ -42,4 +59,45 @@ export const getMostRecentArtifact = async (
   }
 
   return filteredArtifact;
+};
+
+const readArtifact = async (
+  repoOwner: string,
+  repoName: string,
+  authToken: string,
+  artifact: Artifact
+): Promise<string> => {
+  const octokit = new Octokit({
+    auth: authToken,
+  });
+
+  const response = await octokit.request('GET /repos/{owner}/{repo}/actions/artifacts/{id}/zip', {
+    owner: repoOwner,
+    repo: repoName,
+    id: artifact.id,
+  });
+
+  if (response.status !== 200) {
+    throw new Error(`Could not download the artifact (${response.status})`);
+  }
+
+  if (response.headers['content-type'] !== 'application/zip') {
+    throw new Error(`Downloaded artifact content type is wrong (${response.headers['content-type']})`);
+  }
+
+  return await readZipArchive(response.data, artifact.name);
+};
+
+export const readLatestArtifact = async (
+  repoOwner: string,
+  repoName: string,
+  authToken: string,
+  artifactName: string
+): Promise<string | undefined> => {
+  const artifact = await getMostRecentArtifact(repoOwner, repoName, authToken, artifactName);
+  if (!artifact) {
+    return;
+  }
+
+  return await readArtifact(repoOwner, repoName, authToken, artifact);
 };
