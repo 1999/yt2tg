a75773909b3b38f45d9de83542a99539557f56fc
Dmitrii Sorin <dsorin@atlassian.com>
2022-08-14 11:56:53 +1000

    :coneofshame: typo

--- src/cli/telegram-send.ts
+++ src/cli/telegram-send.ts
@@ -1,9 +1,13 @@
 import { info } from '@actions/core';
 import { sendMesage } from '../telegram';
-import {checkEnvironmentVariableSet} from "./environment";
+import { checkEnvironmentVariableSet } from './environment';
 
 async function main() {
-  const [botToken, chatId, fetchedVideos] = checkEnvironmentVariableSet('TELEGRAM_BOT_TOKEN', 'TELEGRAM_CHAT_ID', 'VIDEOS'
+  const [botToken, chatId, fetchedVideos] = checkEnvironmentVariableSet(
+    'TELEGRAM_BOT_TOKEN',
+    'TELEGRAM_CHAT_ID',
+    'VIDEOS'
+  );
   const videos = JSON.parse(fetchedVideos) as string[];
   info(`Videos found: ${videos.length}`);
 
